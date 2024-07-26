c0a7339c4ccb851f43e366413121895a56b1f9e7
Onur Kunaç <onrkunacc@gmail.com>
2024-07-26 17:13:03 +0300

    Start project

---
+++ Portfolio/DAL/Context/PortfolioContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.DAL.Entities;
+
+namespace Portfolio.DAL.Context
+{
+    public class PortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQL2017;Initial Catalog=PortfolioDb;Integrated Security=true;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<MyPortfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+
+}
---
+++ Portfolio/DAL/Entities/MyPortfolio.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.DAL.Entities
+{
+    public class MyPortfolio
+    {
+        [Key]
+        public int PortfolioId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImgUrl { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
